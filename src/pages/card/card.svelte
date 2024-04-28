2a81d0887de05d10d8adb3a9e0d5d24f2fff3a52
Cyborgnetical <Cyborgnetical@outlook.com>
2024-04-28 16:16:29 -0700

    added more

---
+++ src/pages/card/card.svelte
@@ -0,0 +1,32 @@
+
+
+<script lang="ts">
+    export let title:string;
+    export let colors:string;
+    export let angle:number;
+    export let link:string;
+    
+
+</script>
+
+
+    <a href={link}>
+        <div>
+            <img alt="display" style="background: linear-gradient({angle}deg,{colors});">
+            <h3 class="title" style=" background: linear-gradient({angle}deg,{colors}); background-clip: text; color: transparent; ">{title}</h3>
+            <p>
+                <slot />
+            </p>
+        </div>
+    </a>
+    
+
+    <style lang="scss">
+        @import './_styles.scss';
+    </style>
+
+
+
+
+
+
