f003dea05374da12715a846f7b284db09a66d6f3
Cyborgnetical <140975796+Cyborgnetical@users.noreply.github.com>
2025-03-14 07:53:37 -0700

    testing

---
+++ src/content.config.ts
@@ -0,0 +1,31 @@
+// 1. Import utilities from `astro:content`
+import { defineCollection, z } from 'astro:content';
+
+// 2. Import loader(s)
+import { glob, file } from 'astro/loaders';
+
+// 3. Define your collection(s)
+const Notes = defineCollection({ 
+    loader: glob({ base: "./src/content/notes", pattern: '**/*.{md,mdx}' }),
+    schema: z.object({
+        title: z.string(),
+        description: z.string(),
+        pubDate: z.coerce.date(),
+        updatedDate: z.coerce.date().optional(),
+    })
+});
+/*
+const Blog = defineCollection({ 
+    loader: glob({ pattern: "/*.md", base: "./src/content/blog" }),
+    schema: z.object({
+        title: z.string(),
+        description: z.string(),
+        pubDate: z.coerce.date(),
+        updatedDate: z.coerce.date().optional(),
+    })
+});*/
+console.log("hi")
+console.log(Notes)
+
+// 4. Export a single `collections` object to register your collection(s)
+export const collections = { Notes /**Blog */ };
---
+++ src/pages/rss.xml.js
@@ -0,0 +1,15 @@
+import rss from '@astrojs/rss';
+import { getCollection } from 'astro:content';
+
+export async function GET(context) {
+	const posts = await getCollection('Notes');
+	return rss({
+		title: "PlaceHolder",
+		description: "PlaceHolder-Banana",
+		site: context.site,
+		items: posts.map((post) => ({
+			...post.data,
+			link: `/notes/${post.id}/`,
+		})),
+	});
+}
